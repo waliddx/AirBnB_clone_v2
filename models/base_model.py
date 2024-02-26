3f85f0a692008cffd493d16234453c080ed3ba81
waliddx <wagzouly@gmail.com>
2024-02-26 03:26:57 -0800

    x

--- models/base_model.py
+++ models/base_model.py
@@ -19,23 +19,23 @@ else:
     Base = object
 
 
-class BaseModel:
+    class BaseModel:
     """The BaseModel class from which future classes will be derived"""
     if models.storage_t == "db":
         id = Column(String(60), primary_key=True)
         created_at = Column(DateTime, default=datetime.utcnow)
         updated_at = Column(DateTime, default=datetime.utcnow)
 
-    def __init__(self, *args, **kwargs):
+        def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
-            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                    if kwargs.get("created_at", None) and type(self.created_at) is str:
                 self.created_at = datetime.strptime(kwargs["created_at"], time)
             else:
                 self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                if kwargs.get("updated_at", None) and type(self.updated_at) is str:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], time)
 
